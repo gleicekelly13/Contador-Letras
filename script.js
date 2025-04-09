f62aff038ac60609df4192a5af81fae11743d04e
gleicekelly13 <gleice.kelly22@hotmail.com>
2025-04-09 17:24:50 -0300

    Criação da função que conta as letras

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const input = document.getElementById('texto');
 const botaoContar = document.getElementById('botao');
 const resultadoContagem = document.getElementById('resultado');
+
+function contarChar() {
+    let quantidade_char = input.value.trim().length;  
+    resultadoContagem.textContent = `O número de caracteres é: ${quantidade_char}`;
+}
+
+botaoContar.addEventListener('click', contarChar);
