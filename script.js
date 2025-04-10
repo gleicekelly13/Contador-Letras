8bc504328bbc3debe9c11fbcd4873b28ed711e02
gleicekelly13 <gleice.kelly22@hotmail.com>
2025-04-10 11:27:51 -0300

    Remoção de todos os espaços da string

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const botaoContar = document.getElementById('botao');
 const resultadoContagem = document.getElementById('resultado');
 
 function contarChar() {
-    let quantidade_char = input.value.trim().length;  
+    let quantidade_char = input.value.replace(/\s/g, '').length; //Remove os espaços da string e conta os caracteres
     resultadoContagem.textContent = `O número de caracteres é: ${quantidade_char}`;
 }
 
